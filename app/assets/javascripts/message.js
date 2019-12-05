04e27e604782b3efb9d2a8015e7bf37b422e1c62
yuhakare <iheuyo927@yahoo.co.jp>
2019-12-05 23:38:36 +0900

    メッセージ非同期通信実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,26 @@
-$(function() {
-  function buildHTML(message){
-    image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
-  var html = `<div class="message">
-                <div class="upper-message">
-                    <div class="upper-message__user-name">
-                        ${ message.user_name }
-                    </div>
-                    <div class="upper-message__date">
-                        ${ message.date }
-                    </div>
-                </div>
-                <div class="lower-message">
-                    <p class="lower-message__content">
-                        ${ message.content }
-                    </p>
-                    ${image}
-                </div>
-             </div>`
-      return html; 
+$(function () {
+
+  function buildHTML(message) {
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
+
+    var html = `<div class="message" data-message-id="${message.id}"> 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-meesage">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+    return html;
   }
 
   $("#new_message").on('submit', function(e) {
@@ -45,4 +48,34 @@ $(function() {
         $('.form__submit').prop('disabled', false);
     })
   })
-})
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id"); 
+    $.ajax({
+      //ルーティングで設定した通りのURLを指
+      url: "api/messages", 
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+});
+
+
